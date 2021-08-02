161cfac13fdc0f4d24fc0a3eb6addae3fd121e72
Alessandro <alessandro.molinaro96@gmail.com>
2021-08-02 22:49:45 +0200

    Initial commit

---
+++ src/main/java/com/alessandro/socialnetworkauthserver/SocialnetworkAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.alessandro.socialnetworkauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialnetworkAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialnetworkAuthServerApplication.class, args);
+    }
+
+}
